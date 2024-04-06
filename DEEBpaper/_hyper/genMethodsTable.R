847c2d076b70d0bbfa2245dcc5c96d6e35aafdec
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-04-06 16:13:20 +0200

    new hyper files

--- DEEBpaper/_hyper/genMethodsTable.R
+++ DEEBpaper/_hyper/genMethodsTable.R
@@ -9,14 +9,16 @@ methodsTable <-
   ) |>
     dplyr::mutate(obs = obsNames[model]) |>
     tidyr::unnest_longer(obs) |>
-    dplyr::mutate(timeInMinutes = 60) |> 
+    dplyr::mutate(timeInMinutes = 10) |>
   dplyr::mutate(
     endNr = stringr::str_extract(method, "(?<=-)\\d+$") |> as.integer(),
-    interp = stringr::str_detect(method, "(?<=-)interp"),
-    isLorenzian = stringr::str_detect(model, "lorenz"),
-    fxd = stringr::str_detect(obs, "^fxd_"),
-    noisy = stringr::str_detect(obs, "noisy$")) |> 
-  dplyr::filter(is.na(endNr) | (isLorenzian & endNr >= 100) | (!isLorenzian & endNr < 100)) |> 
-  dplyr::filter(!interp | !noisy) |> 
+    isLorenzian = stringr::str_detect(model, "_lorenz\\b"),
+    fxd = stringr::str_detect(obs, "_fxd\\b"),
+    rnd = stringr::str_detect(obs, "_rnd\\b"),
+    interp = stringr::str_detect(method, "_interp\\b"),
+    noisy = stringr::str_detect(obs, "_noisy\\b")
+  ) |>
+  dplyr::filter(is.na(endNr) | (isLorenzian & endNr >= 100) | (!isLorenzian & endNr < 100)) |>
+  dplyr::filter(!interp | !noisy) |>
   dplyr::select(model, method, obs, timeInMinutes)
 readr::write_csv(methodsTable, "methods.csv")
