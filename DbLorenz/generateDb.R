9d803df4d1d2bef506260fcc402e07fa87a94d72
Christof Schötz <math@christof-schoetz.de>
2024-06-24 21:33:36 +0200

    blub

--- DbLorenz/generateDb.R
+++ DbLorenz/generateDb.R
@@ -1,3 +1,4 @@
+deebExtrasPath <- "~/DEEBextras"
 sourceBasePath <- "~/DEEBextras/DbLorenz"
 targetBasePath <- "~/DeebDbLorenz"
 
@@ -10,6 +11,7 @@ library(DEEBdata)
 tuneDbPath <- paste0(targetBasePath, "Tune")
 testDbPath <- paste0(targetBasePath, "Test")
 hyperTemplatePath <- file.path(sourceBasePath, "hyper")
+hyperTemplate2Path <- file.path(deebExtrasPath, "hyper")
 
 
 
@@ -29,6 +31,10 @@ tuneHyperPath <- file.path(tuneDbPath, "_hyper")
 dir.create(tuneHyperPath)
 cat("Copying hyper files from", hyperTemplatePath, "to", tuneHyperPath, "... ")
 for (path in dir(hyperTemplatePath, full.names=TRUE)) {
+  file.copy(from = path, to = tuneHyperPath, recursive = TRUE, overwrite = TRUE)
+}
+cat("Copying hyper files from", hyperTemplate2Path, "to", tuneHyperPath, "... ")
+for (path in dir(hyperTemplate2Path, full.names=TRUE)) {
   file.copy(from = path, to = tuneHyperPath, recursive = TRUE, overwrite = TRUE)
 }
 cat("Done.\n")
